50436357e242c95d3221c49c8f93ba2245e07681
Tony Chow <tchow9@gmail.com>
2023-07-15 10:18:13 -0700

    Add driver and customer seed data

--- database/src/db/seeds/customer_seed.sql
+++ database/src/db/seeds/customer_seed.sql
@@ -1,2 +1,5 @@
 INSERT INTO customers (customer_name, customer_email, customer_street_address, city, postal_code, phone)
 VALUES ('Clement Shum', 'clement.shum@gmail.com', '5540 Forest St', 'Burnaby, BC', 'V5G 3X4', '416 155 1455');
+
+INSERT INTO customers (customer_name, customer_email, customer_street_address, city, postal_code, phone)
+VALUES ('Tony Chow', 'fakeemail@gmail.com', '4700 Kingsway', 'Burnaby, BC', 'V5H 4M1', '604 439 1331');
--- database/src/db/seeds/driver_seed.sql
+++ database/src/db/seeds/driver_seed.sql
@@ -1,2 +1,5 @@
 INSERT INTO drivers (orders_id, driver_name, driver_phone)
 VALUES (1, 'Kyle Burgerson', '614 552 3377');
+
+INSERT INTO drivers (orders_id, driver_name, driver_phone)
+VALUES (2, 'Bobby Flay', '780 876 6520');
