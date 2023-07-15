bfdd36d2841b81119e64d0035525393f2f19f3eb
Denny Ng <Dennyng1989@hotmail.com>
2023-07-15 11:22:52 -0700

    Add dummy driver and customer data

--- database/src/db/seeds/customer_seed.sql
+++ database/src/db/seeds/customer_seed.sql
@@ -3,3 +3,6 @@ VALUES ('Clement Shum', 'clement.shum@gmail.com', '5540 Forest St', 'Burnaby, BC
 
 INSERT INTO customers (customer_name, customer_email, customer_street_address, city, postal_code, phone)
 VALUES ('Tony Chow', 'fakeemail@gmail.com', '4700 Kingsway', 'Burnaby, BC', 'V5H 4M1', '604 439 1331');
+
+INSERT INTO customers (customer_name, customer_email, customer_street_address, city, postal_code, phone)
+VALUES ('Denny Ng', 'Dennis@menace.com', '4331 Dominion St', 'Burnaby' 'BC V5G 1C7');
--- database/src/db/seeds/driver_seed.sql
+++ database/src/db/seeds/driver_seed.sql
@@ -1,5 +1,10 @@
+-- DN I don't think we're supposed to have orders_id in here, but I may be wrong --
+
 INSERT INTO drivers (orders_id, driver_name, driver_phone)
 VALUES (1, 'Kyle Burgerson', '614 552 3377');
 
 INSERT INTO drivers (orders_id, driver_name, driver_phone)
 VALUES (2, 'Bobby Flay', '780 876 6520');
+
+INSERT INTO drives (orders_id, drive_name, driver_phone)
+VALUES (3, 'Jimmy Koerner', '236 142 4695');
