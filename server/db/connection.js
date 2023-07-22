6490ec4ff080423aa9a77970e9d0aa0df7273ee6
Tony Chow <tchow9@gmail.com>
2023-07-21 18:03:25 -0700

    Add database connection and test code

--- server/db/connection.js
+++ server/db/connection.js
@@ -1,16 +1,18 @@
 // PG database client/connection setup
-const { Pool } = require('pg');
+const { Pool } = require("pg");
 
 const dbParams = {
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
-  database: process.env.DB_NAME
+  database: process.env.DB_NAME,
 };
 
 const db = new Pool(dbParams);
 
-db.connect();
+db.connect().then(() => {
+  console.log("Connected to database!");
+});
 
 module.exports = db;
--- server/server.js
+++ server/server.js
@@ -1,51 +1,69 @@
 // load .env data into process.env
-require('dotenv').config();
+require("dotenv").config();
 
 // Web server config
-const sassMiddleware = require('./lib/sass-middleware');
-const express = require('express');
-const morgan = require('morgan');
-
+const sassMiddleware = require("./lib/sass-middleware");
+const express = require("express");
+const morgan = require("morgan");
+const db = require("./db/connection");
 const PORT = process.env.PORT || 8080;
 const app = express();
-
-app.set('view engine', 'ejs');
+// const cookieSession = require("cookie-session");
+// app.use(
+//   cookieSession({
+//     name: "session",
+//     keys: ["key1"],
+//   })
+// );
+// app.set("view engine", "ejs");
 
 // Load the logger first so all (static) HTTP requests are logged to STDOUT
 // 'dev' = Concise output colored by response status for development use.
 //         The :status token will be colored red for server error codes, yellow for client error codes, cyan for redirection codes, and uncolored for all other codes.
-app.use(morgan('dev'));
-app.use(express.urlencoded({ extended: true }));
-app.use(
-  '/styles',
-  sassMiddleware({
-    source: __dirname + '/styles',
-    destination: __dirname + '/public/styles',
-    isSass: false, // false => scss, true => sass
-  })
-);
-app.use(express.static('public'));
+// app.use(morgan("dev"));
+// app.use(express.urlencoded({ extended: true }));
+// app.use(
+//   "/styles",
+//   sassMiddleware({
+//     source: __dirname + "/styles",
+//     destination: __dirname + "/public/styles",
+//     isSass: false, // false => scss, true => sass
+//   })
+// );
+// app.use(express.static("public"));
 
 // Separated Routes for each Resource
 // Note: Feel free to replace the example routes below with your own
-const userApiRoutes = require('./routes/users-api');
-const widgetApiRoutes = require('./routes/widgets-api');
-const usersRoutes = require('./routes/users');
+// const userApiRoutes = require("./routes/users-api");
+// const widgetApiRoutes = require("./routes/widgets-api");
+// const usersRoutes = require("./routes/users");
 
 // Mount all resource routes
 // Note: Feel free to replace the example routes below with your own
 // Note: Endpoints that return data (eg. JSON) usually start with `/api`
-app.use('/api/users', userApiRoutes);
-app.use('/api/widgets', widgetApiRoutes);
-app.use('/users', usersRoutes);
+// app.use("/api/users", userApiRoutes);
+// app.use("/api/widgets", widgetApiRoutes);
+// app.use("/users", usersRoutes);
 // Note: mount other resources here, using the same pattern above
 
 // Home page
 // Warning: avoid creating more routes in this file!
 // Separate them into separate routes files (see above).
 
-app.get('/', (req, res) => {
-  res.render('index');
+//the query
+// const theQuery = `
+//   SELECT * FROM restaurants
+//   WHERE restaurants_id = 1;
+//   `;
+
+const insertValue = `INSERT INTO restaurants (email, restaurant_street_address, city, postal_code, phone, category)
+VALUES ('Billys@gmail.com', '230-4700 Kingsway', 'Burnaby, BC', 'V5H 4N2', '604 423 9400', 'malaysian');`;
+
+app.get("/", (req, res) => {
+  // res.render("index");
+  db.query(insertValue, (err, result) => {
+    res.send("Hello");
+  });
 });
 
 app.listen(PORT, () => {
