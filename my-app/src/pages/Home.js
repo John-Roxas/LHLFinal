9b2fc9296a2e06aa54856c4fadd432a824027524
Tony Chow <tchow9@gmail.com>
2023-07-22 11:53:11 -0700

    Add restaurant props passed from useEffect App.js

--- my-app/src/pages/Home.js
+++ my-app/src/pages/Home.js
@@ -2,11 +2,11 @@ import SearchBar from "../components/SearchBar";
 import NavigationBar from "../components/NavigationBar";
 import RestaurantList from "../components/RestaurantList";
 
-function Index() {
+function Index(props) {
   return (
     <div className="App">
       <SearchBar />
-      <RestaurantList />
+      <RestaurantList restaurant={props.restaurant} />
       <NavigationBar />
     </div>
   );
