35fd5af229bbe3784d3f4655270e08455f62d984
Tony Chow <tchow9@gmail.com>
2023-07-23 14:33:03 -0700

    Add divs to restaurant list item

--- my-app/src/components/RestaurantListItem.js
+++ my-app/src/components/RestaurantListItem.js
@@ -2,28 +2,33 @@ import { Link } from "react-router-dom";
 
 export default function RestaurantListItem(props) {
   return (
-    <article>
-      <a>
+    <article className="restaurant-item">
+      <div>
+        <img
+          src={props.picture}
+          alt={props.name}
+          onClick={console.log("Clicked")}
+        />
+      </div>
+      <div>
         <div>
-          <img
-            src={props.picture}
-            alt={props.name}
-            onClick={console.log("Clicked")}
-          />
-        </div>
-        <h3>
           <span>{props.name}</span>
-        </h3>
-
-        <Link
-          to={`/restaurant/${props.id}`}
-          state={{ name: props.name, food: props.food }}
-        >
-          <button class="btn" type="submit" onClick={console.log(1)}>
-            Visit
-          </button>
-        </Link>
-      </a>
+        </div>
+        <div>
+          <p>The description</p>
+        </div>
+        <a>
+          <Link
+            to={`/restaurant/${props.id}`}
+            state={{ name: props.name, food: props.food }}
+          >
+            <button class="btn" type="submit" onClick={console.log(1)}>
+              Visit
+            </button>
+          </Link>
+        </a>
+      </div>
+      <h3></h3>
     </article>
   );
 }
