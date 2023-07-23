cd911686f919a17287b5b1e962c45b5bcd44dd89
Tony Chow <tchow9@gmail.com>
2023-07-23 16:07:16 -0700

    Add classNames to div

--- my-app/src/components/RestaurantList.js
+++ my-app/src/components/RestaurantList.js
@@ -4,13 +4,15 @@ import "./RestaurantList.css";
 export default function RestaurantList(props) {
   const restaurants = props.restaurant.map((restaurant) => {
     return (
-      <RestaurantListItem
-        key={restaurant.id}
-        id={restaurant.id}
-        name={restaurant.restaurant_name}
-        picture={restaurant.restaurant_picture}
-        // food={restaurant.food}
-      />
+      <div className="restaurant-list">
+        <RestaurantListItem
+          key={restaurant.id}
+          id={restaurant.id}
+          name={restaurant.restaurant_name}
+          picture={restaurant.restaurant_picture}
+          // food={restaurant.food}
+        />
+      </div>
     );
   });
 
