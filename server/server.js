8b37dfa7568372626ad36095f22d153777d04f68
Tony Chow <tchow9@gmail.com>
2023-07-24 16:59:48 -0700

    Relocate server request to routes and queries

--- server/server.js
+++ server/server.js
@@ -26,59 +26,28 @@ app.use(
 // Load the logger first so all (static) HTTP requests are logged to STDOUT
 // 'dev' = Concise output colored by response status for development use.
 //         The :status token will be colored red for server error codes, yellow for client error codes, cyan for redirection codes, and uncolored for all other codes.
-// app.use(morgan("dev"));
-// app.use(
-//   "/styles",
-//   sassMiddleware({
-//     source: __dirname + "/styles",
-//     destination: __dirname + "/public/styles",
-//     isSass: false, // false => scss, true => sass
-//   })
-// );
-// app.use(express.static("public"));
+app.use(morgan("dev"));
+app.use(
+  "/styles",
+  sassMiddleware({
+    source: __dirname + "/styles",
+    destination: __dirname + "/public/styles",
+    isSass: false, // false => scss, true => sass
+  })
+);
+app.use(express.static("public"));
 
 // Separated Routes for each Resource
 // Note: Feel free to replace the example routes below with your own
-// const userApiRoutes = require("./routes/users-api");
-// const widgetApiRoutes = require("./routes/widgets-api");
-// const usersRoutes = require("./routes/users");
+const restaurantListRoutes = require("./routes/restaurantListRoutes");
 
 // Mount all resource routes
 // Note: Feel free to replace the example routes below with your own
 // Note: Endpoints that return data (eg. JSON) usually start with `/api`
-// app.use("/api/users", userApiRoutes);
-// app.use("/api/widgets", widgetApiRoutes);
-// app.use("/users", usersRoutes);
-// Note: mount other resources here, using the same pattern above
-
+app.use("/api/restaurants", restaurantListRoutes);
 // Home page
 // Warning: avoid creating more routes in this file!
 // Separate them into separate routes files (see above).
-
-//the query
-const theQuery = `
-  SELECT * FROM restaurants;
-  `;
-
-const visitRestaurant = `SELECT * FROM food_items WHERE restaurants_id = 1`;
-// const insertValue = `INSERT INTO restaurants (restaurant_name,email, restaurant_street_address, city, postal_code, phone, category)
-// VALUES ('Amazing Restaurant','DennysRestaurant@gmail.com', '230-4700 Kingsway', 'Burnaby, BC', 'V5H 4N2', '604 423 9400', 'malaysian');`;
-
-app.get("/api/restaurants", (req, res) => {
-  // res.render("index");
-  db.query(theQuery, (err, result) => {
-    // res.send(result);
-    // console.log(result.rows);
-    res.send(result.rows);
-    // res.send("hello");
-  });
-});
-
-app.get("/restaurant/1", (req, res) => {
-  db.query(visitRestaurant, (err, result) => {
-    res.send(result.rows);
-  });
-});
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
