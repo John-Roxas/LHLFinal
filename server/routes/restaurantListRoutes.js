1e8f2d2117c290f728c60f99af6d5602127a5800
Tony Chow <tchow9@gmail.com>
2023-07-24 17:00:09 -0700

    Add restaurantList route and query

---
+++ server/routes/restaurantListRoutes.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const router = express.Router();
+const db = require("../db/connection");
+const { getRestaurantList } = require("../db/queries/restuarantList");
+
+router.get("/", (req, res) => {
+  const restaurantQuery = getRestaurantList();
+  db.query(restaurantQuery)
+    .then((result) => {
+      res.send(result.rows);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.sendStatus(500);
+    });
+});
+
+module.exports = router;
