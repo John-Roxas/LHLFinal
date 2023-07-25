58f2e59efc11f0b4cdc0b043c4b9232c8dc5c74e
Tony Chow <tchow9@gmail.com>
2023-07-25 14:28:50 -0700

    Remove login component and page code

--- my-app/src/pages/Home.js
+++ my-app/src/pages/Home.js
@@ -1,7 +1,6 @@
 import SearchBar from "../components/SearchBar";
 import NavigationBar from "../components/NavigationBar";
 import RestaurantList from "../components/RestaurantList";
-import Login from "../components/LoginForm";
 
 function Index(props) {
   return (
