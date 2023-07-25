fb022ff4f10860092aafd412501a241f1c5186ba
Tony Chow <tchow9@gmail.com>
2023-07-25 14:26:58 -0700

    Add Login button and form to search container

--- my-app/src/components/SearchBar.js
+++ my-app/src/components/SearchBar.js
@@ -1,32 +1,83 @@
 import { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
+import { faRectangleXmark } from "@fortawesome/free-solid-svg-icons";
 import "./SearchBar.css";
 
 function SearchBar() {
   const [restuarantSearch, setResturantSearch] = useState("");
+  const [showLoginPopup, setShowLoginPopup] = useState(false);
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
 
-  const onFormSubmit = (e) => {
-    e.preventDefault();
+  const handleLoginClick = () => {
+    setShowLoginPopup(true);
   };
 
-  const eventHandler = (e) => {
-    setResturantSearch(e.target.value);
+  const closeLoginPopup = () => {
+    setShowLoginPopup(false);
+  };
+
+  const handleLoginSubmit = (event) => {
+    event.preventDefault();
+    console.log("Username:", username);
+    console.log("Passowrd", password);
+    closeLoginPopup();
+  };
+
+  const onFormSubmit = (event) => {
+    event.preventDefault();
+  };
+
+  const eventHandler = (event) => {
+    setResturantSearch(event.target.value);
   };
 
   return (
-    <div className="search-container">
-      <form className="form-field" onSubmit={onFormSubmit}>
-        <FontAwesomeIcon icon={faMagnifyingGlass} />
-        <input
-          className="search-bar"
-          name="restuarantSearch"
-          type="text"
-          placeholder="Food, restuarant, drinks, etc."
-          value={restuarantSearch}
-          onChange={eventHandler}
-        />
-      </form>
+    <div>
+      <div className="search-container">
+        <form className="form-field" onSubmit={onFormSubmit}>
+          <FontAwesomeIcon icon={faMagnifyingGlass} />
+          <input
+            className="search-bar"
+            name="restuarantSearch"
+            type="text"
+            placeholder="Food, restaurant, drinks, etc."
+            value={restuarantSearch}
+            onChange={eventHandler}
+          />
+        </form>
+        <div className="login-container">
+          <button className="login-button" onClick={handleLoginClick}>
+            Login
+          </button>
+          {showLoginPopup && (
+            <>
+              <div className="overlay" onClick={closeLoginPopup} />
+              <div className="login-popup">
+                <form className="login-form" onSubmit={handleLoginSubmit}>
+                  <input
+                    type="text"
+                    placeholder="Username"
+                    value={username}
+                    onChange={(event) => setUsername(event.target.value)}
+                  />
+                  <input
+                    type="password"
+                    placeholder="Password"
+                    value={password}
+                    onChange={(event) => setPassword(event.target.value)}
+                  />
+                  <button type="submit">Submit</button>
+                </form>
+                <button className="close-button" onClick={closeLoginPopup}>
+                  <FontAwesomeIcon icon={faRectangleXmark} />
+                </button>
+              </div>
+            </>
+          )}
+        </div>
+      </div>
     </div>
   );
 }
