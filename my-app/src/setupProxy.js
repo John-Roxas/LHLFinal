13853ccddc2071e77199a66e5ae3fcd7fd610279
Tony Chow <tchow9@gmail.com>
2023-07-25 19:01:29 -0700

    Add proxy path to proxy middleware

--- my-app/src/setupProxy.js
+++ my-app/src/setupProxy.js
@@ -2,6 +2,13 @@ const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = function (app) {
   app.use(
+    "/api/restaurants",
+    createProxyMiddleware({
+      target: "http://localhost:8080",
+      changeOrigin: true,
+    })
+  );
+  app.use(
     "/login",
     createProxyMiddleware({
       target: "http://localhost:8080",
