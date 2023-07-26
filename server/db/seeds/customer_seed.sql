e3c4f61e367353d2b16bb2dcbb9c58c77e7218cc
Tony Chow <tchow9@gmail.com>
2023-07-25 19:00:19 -0700

    Changed email to test in Tony seed for easy login

--- server/db/seeds/customer_seed.sql
+++ server/db/seeds/customer_seed.sql
@@ -2,7 +2,7 @@ INSERT INTO customers (customer_name, customer_email, customer_street_address, c
 VALUES ('Clement Shum', 'clement.shum@gmail.com', '5540 Forest St', 'Burnaby, BC', 'V5G 3X4', '416 155 1455', 'test');
 
 INSERT INTO customers (customer_name, customer_email, customer_street_address, city, postal_code, phone, customer_password)
-VALUES ('Tony Chow', 'test@test.com', '4700 Kingsway', 'Burnaby, BC', 'V5H 4M1', '604 439 1331', 'test');
+VALUES ('Tony Chow', 'test', '4700 Kingsway', 'Burnaby, BC', 'V5H 4M1', '604 439 1331', 'test');
 
 INSERT INTO customers (customer_name, customer_email, customer_street_address, city, postal_code, phone, customer_password)
 VALUES ('Denny Ng', 'Dennis@menace.com', '4331 Dominion St', 'Burnaby, BC', 'V5G 1C7', '234 423 3423', 'test');
