37da906ef41d634dad675730b1930a776f3c1af7
Denny Ng <Dennyng1989@hotmail.com>
2023-07-25 23:57:21 -0700

    Impl. restaurant search queries

---
+++ server/db/queries/search.js
@@ -0,0 +1,36 @@
+const db = require("../connection");
+
+const getRestaurants = (mode, searchTerm) => {
+  // Base query string
+  let queryString = "";
+  
+  // Query for searching by food item name
+  if (mode == "name") {
+    queryString += `
+      SELECT DISTINCT restaurants.* FROM restaurants
+      JOIN food_items ON food_items.restaurants_id = restaurants.id
+      WHERE food_name ILIKE $1
+      ORDER BY restaurants.id;
+    `
+  } else if (mode == "category") {
+    // Query mode for searching by category
+    queryString += `
+    SELECT * FROM restaurants
+    WHERE category ILIKE $1;`
+  } else {
+    // Default query mode, 
+    queryString += `
+    SELECT * FROM restaurants
+    WHERE restaurant_name ILIKE $1`
+  }
+
+  const values = [`%${searchTerm}%` || '%%']
+
+  return db
+    .query(queryString, values)
+    .then((data) => {
+    return data.rows;
+  });
+};
+
+module.exports = { getRestaurants };
