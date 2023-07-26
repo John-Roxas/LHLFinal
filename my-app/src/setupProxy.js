8b5dfec38da4c897f53aa72c621dac5172d4729c
Tony Chow <tchow9@gmail.com>
2023-07-25 20:09:30 -0700

    May not need this setupProxy file

--- my-app/src/setupProxy.js
+++ my-app/src/setupProxy.js
@@ -1,18 +1,18 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = function (app) {
-  app.use(
-    "/api/restaurants",
-    createProxyMiddleware({
-      target: "http://localhost:8080",
-      changeOrigin: true,
-    })
-  );
-  app.use(
-    "/login",
-    createProxyMiddleware({
-      target: "http://localhost:8080",
-      changeOrigin: true,
-    })
-  );
+  // app.use(
+  //   "/api/restaurants",
+  //   createProxyMiddleware({
+  //     target: "http://localhost:8080",
+  //     changeOrigin: true,
+  //   })
+  // );
+  // app.use(
+  //   "/login",
+  //   createProxyMiddleware({
+  //     target: "http://localhost:8080",
+  //     changeOrigin: true,
+  //   })
+  // );
 };
