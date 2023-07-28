c60c8104c154a201b4d52955f3b3999806bcf656
Tony Chow <tchow9@gmail.com>
2023-07-27 17:45:08 -0700

    Add Login button

--- my-app/src/pages/Profile.js
+++ my-app/src/pages/Profile.js
@@ -1,13 +1,20 @@
 // import SearchBar from "../components/SearchBar";
 import NavigationBar from "../components/NavigationBar";
+import useLogin from "../hooks/useLogin";
 
 function Profile() {
+  const { handleLogout } = useLogin();
   return (
     <div className="App">
       <div className="profile-container">
-        <div className="profile-avatar">Picture</div>
-        <div>Customer info</div>
-        <div>List</div>
+        <div className="tile-item">Picture</div>
+        <div className="tile-item">Customer info</div>
+        <div className="tile-item">List</div>
+        <div className="tile-item">
+          <button className="login-button" onClick={handleLogout}>
+            Logout
+          </button>
+        </div>
       </div>
       {/* <h2 className="profile-container"></h2> */}
       <NavigationBar />
