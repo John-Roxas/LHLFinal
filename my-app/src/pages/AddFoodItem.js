3b6fa3df3366c17935dd81e9e9d74c6d9519f6b5
Clement Shum <clement.shum128@gmail.com>
2023-07-29 15:03:40 -0400

    add food item back button

--- my-app/src/pages/AddFoodItem.js
+++ my-app/src/pages/AddFoodItem.js
@@ -1,18 +1,18 @@
-import { useLocation, useParams } from "react-router-dom";
+import { useLocation, useParams, useNavigate } from "react-router-dom";
 import NavigationBar from "../components/NavigationBar";
 import AddFood from "../components/AddFood";
 import { useEffect, useState } from "react";
 import axios from "axios";
+// import "./AddFoodItem.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowLeft } from "@fortawesome/free-solid-svg-icons";
 
 function AddFoodItem(props) {
   const location = useLocation();
   const { foodId } = location.state;
 
-  const { id, foodItemId } = useParams();
-
-  // console.log("AddFootItem.js");
-  // console.log(id);
-  // console.log(foodItemId);
+  const { id, foodItemId } = useParams(); // use the id and foodItemId for useEffect
+  const history = useNavigate(); // this is used to go back to previous page
 
   const [state, setState] = useState({
     foodInfo: [],
@@ -22,7 +22,7 @@ function AddFoodItem(props) {
     axios
       .get(`http://localhost:8080/restaurants/${id}/food_items/${foodItemId}`)
       .then((res) => {
-        console.log("test", res);
+        // console.log("test", res);
         setState((prev) => ({
           ...prev,
           foodInfo: res.data,
@@ -31,12 +31,11 @@ function AddFoodItem(props) {
       .catch((err) => console.log(err));
   }, [id, foodItemId]);
 
-  console.log("id", id);
-  console.log("foodItemId", foodItemId);
-  console.log(state.foodInfo);
-
   return (
     <div>
+      <button onClick={() => history(-1)} className="back-button">
+        <FontAwesomeIcon icon={faArrowLeft} />
+      </button>
       <AddFood foodId={foodId} foodInfo={state.foodInfo} cart={props.cart} />
       <NavigationBar />
     </div>
@@ -44,3 +43,50 @@ function AddFoodItem(props) {
 }
 
 export default AddFoodItem;
+
+// import { useLocation, useParams } from "react-router-dom";
+// import NavigationBar from "../components/NavigationBar";
+// import AddFood from "../components/AddFood";
+// import { useEffect, useState } from "react";
+// import axios from "axios";
+
+// function AddFoodItem(props) {
+//   const location = useLocation();
+//   const { foodId } = location.state;
+
+//   const { id, foodItemId } = useParams();
+
+//   // console.log("AddFootItem.js");
+//   // console.log(id);
+//   // console.log(foodItemId);
+
+//   const [state, setState] = useState({
+//     foodInfo: [],
+//   });
+
+//   useEffect(() => {
+//     axios
+//       .get(`http://localhost:8080/restaurants/${id}/food_items/${foodItemId}`)
+//       .then((res) => {
+//         console.log("test", res);
+//         setState((prev) => ({
+//           ...prev,
+//           foodInfo: res.data,
+//         }));
+//       })
+//       .catch((err) => console.log(err));
+//   }, [id, foodItemId]);
+
+//   console.log("id", id);
+//   console.log("foodItemId", foodItemId);
+//   console.log(state.foodInfo);
+
+//   return (
+//     <div>
+//       <AddFood foodId={foodId} foodInfo={state.foodInfo} cart={props.cart} />
+//       <NavigationBar />
+//     </div>
+//   );
+// }
+
+// export default AddFoodItem;
