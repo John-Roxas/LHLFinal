dde966581d014feda09688a5f2daaa607457181f
Clement Shum <clement.shum128@gmail.com>
2023-07-29 14:24:54 -0400

    update

--- my-app/src/components/RestaurantListItem.js
+++ my-app/src/components/RestaurantListItem.js
@@ -24,9 +24,7 @@ export default function RestaurantListItem(props) {
               picture: props.picture,
             }}
           >
-
             <button class="btn" type="submit">
-
               Visit
             </button>
           </Link>
@@ -36,3 +34,42 @@ export default function RestaurantListItem(props) {
     </article>
   );
 }
+
+// import { Link } from "react-router-dom";
+// import "./RestaurantListItem.css";
+
+// export default function RestaurantListItem(props) {
+//   return (
+//     <article className="tile-item">
+//       <div>
+//         <img src={props.picture} alt={props.name} />
+//       </div>
+//       <div className="restaurant-desc">
+//         <div>
+//           <h1>{props.name}</h1>
+//         </div>
+//         <div>
+//           <p>Delivery fee</p>
+//         </div>
+//         <a>
+//           {/* state passes information to be used in `/restaurants/${props.id}` end point */}
+//           <Link
+//             to={`/restaurants/${props.id}`}
+//             state={{
+//               restaurantId: props.id,
+//               name: props.name,
+//               picture: props.picture,
+//             }}
+//           >
+
+//             <button class="btn" type="submit">
+
+//               Visit
+//             </button>
+//           </Link>
+//         </a>
+//       </div>
+//       <h3></h3>
+//     </article>
+//   );
+// }
