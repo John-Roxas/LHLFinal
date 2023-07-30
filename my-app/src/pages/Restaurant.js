e9b6d76c942b8792151ab24d2d731c45e6dae8f2
Clement Shum <clement.shum128@gmail.com>
2023-07-30 12:13:30 -0400

    restaurant js searchfield

--- my-app/src/pages/Restaurant.js
+++ my-app/src/pages/Restaurant.js
@@ -27,7 +27,7 @@ function Restaurant(props) {
       .catch((err) => console.log(err));
   }, [restaurantId]);
 
-  // console.log("restaurantId", restaurantId);
+  console.log("restaurantId", restaurantId);
   return (
     <div className="App">
       <SearchField />
@@ -36,11 +36,7 @@ function Restaurant(props) {
           <img src={picture} alt={name} className="restaurant-picture" />
         </div>
         <h2>{name}</h2>
-        <FoodList
-          restaurantId={restaurantId}
-          foods={state.foodList}
-          restaurantName={name} //Grabbed from useLocation() on Line 11 and 12
-        />
+        <FoodList restaurantId={restaurantId} foods={state.foodList} />
       </div>
       <NavigationBar />
     </div>
@@ -48,49 +44,3 @@ function Restaurant(props) {
 }
 
 export default Restaurant;
-// import axios from "axios";
-// import { useState, useEffect } from "react";
-// import { useLocation } from "react-router-dom";
-// import SearchField from "../components/SearchField";
-// import NavigationBar from "../components/NavigationBar";
-// import FoodList from "../components/FoodList";
-// import "./Restaurant.css";
-
-// function Restaurant(props) {
-//   // These two take information passed from /src/components/RestaurantListItem
-//   const location = useLocation();
-//   const { restaurantId, name, picture } = location.state;
-
-//   const [state, setState] = useState({
-//     foodList: [],
-//   });
-
-//   useEffect(() => {
-//     axios
-//       .get(`http://localhost:8080/restaurants/${restaurantId}`)
-//       .then((res) => {
-//         setState((prev) => ({
-//           ...prev,
-//           foodList: res.data,
-//         }));
-//       })
-//       .catch((err) => console.log(err));
-//   }, [restaurantId]);
-
-//   console.log("restaurantId", restaurantId);
-//   return (
-//     <div className="App">
-//       <SearchField />
-//       <div className="restaurant-foods-container">
-//         <div>
-//           <img src={picture} alt={name} className="restaurant-picture" />
-//         </div>
-//         <h2>{name}</h2>
-//         <FoodList restaurantId={restaurantId} foods={state.foodList} />
-//       </div>
-//       <NavigationBar />
-//     </div>
-//   );
-// }
-
-// export default Restaurant;
