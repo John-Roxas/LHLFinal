cc07df78068803c798b59ca5817d9a2ce5ca0dee
Tony Chow <tchow9@gmail.com>
2023-07-30 14:57:20 -0700

    Add props to show customerInfo

--- my-app/src/pages/Profile.js
+++ my-app/src/pages/Profile.js
@@ -1,13 +1,14 @@
 import NavigationBar from "../components/NavigationBar";
 import useLogin from "../hooks/useLogin";
 
-function Profile() {
+function Profile(props) {
+  console.log(props.customerInfo);
   const { handleLogout } = useLogin();
   return (
     <div className="App">
       <div className="profile-container">
-        <div className="tile-item">Picture</div>
-        <div className="tile-item">Customer info</div>
+        <div className="tile-item">{props.customerInfo.customer_avatar}</div>
+        <div className="tile-item">{props.customerInfo.customer_name}</div>
         <div className="tile-item">List</div>
         <div className="tile-item">
           <button className="login-button" onClick={handleLogout}>
