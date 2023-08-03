4406b742d6154620cb446c380014e93e8318ae02
Tony Chow <tchow9@gmail.com>
2023-08-02 20:43:29 -0700

    Fix orders schema and add orders seed data

--- server/db/seeds/test_order_seed.sql
+++ server/db/seeds/test_order_seed.sql
@@ -1,12 +1,35 @@
-INSERT INTO orders (customers_id, drivers_id, restaurants_id, food_items_id, food_items_quantity, food_items_price, food_name)
-VALUES (1, 2, 3, 1, 2, 12.33, 'Burger');
+INSERT INTO orders (date, customers_id, drivers_id, restaurants_id, food_items_id, food_items_quantity, food_items_price, food_name)
+VALUES ('2023-07-01 10:15:00', 1, 2, 3, 1, 2, 12.33, 'Burger');
 
-INSERT INTO orders (customers_id, drivers_id, restaurants_id, food_items_id, food_items_quantity, food_items_price, food_name)
-VALUES (1, 2, 3, 1, 2, 12.33, 'Fries');
+INSERT INTO orders (date, customers_id, drivers_id, restaurants_id, food_items_id, food_items_quantity, food_items_price, food_name)
+VALUES ('2023-07-01 11:30:00', 1, 2, 3, 1, 2, 12.33, 'Fries');
 
-INSERT INTO orders (customers_id, drivers_id, restaurants_id, food_items_id, food_items_quantity, food_items_price, food_name)
-VALUES (1, 2, 3, 1, 2,12.33, 'Nuggets');
+INSERT INTO orders (date, customers_id, drivers_id, restaurants_id, food_items_id, food_items_quantity, food_items_price, food_name)
+VALUES ('2023-07-01 12:45:00', 1, 2, 3, 1, 2, 12.33, 'Nuggets');
 
-INSERT INTO orders (customers_id, drivers_id, restaurants_id, food_items_id, food_items_quantity, food_items_price, food_name)
-VALUES (1, 2, 3, 1, 2, 12.33, 'Pop');
+INSERT INTO orders (date, customers_id, drivers_id, restaurants_id, food_items_id, food_items_quantity, food_items_price, food_name)
+VALUES ('2023-07-02 09:30:00', 1, 2, 3, 1, 2, 12.33, 'Pop');
 
+INSERT INTO orders (date, customers_id, drivers_id, restaurants_id, food_items_id, food_items_quantity, food_items_price, food_name)
+VALUES ('2023-07-02 10:45:00', 1, 2, 1, 1, 1, 12.33, 'pizza');
+
+INSERT INTO orders (date, customers_id, drivers_id, restaurants_id, food_items_id, food_items_quantity, food_items_price, food_name)
+VALUES ('2023-07-02 12:00:00', 1, 2, 1, 1, 1, 12.33, 'garlic bread');
+
+INSERT INTO orders (date, customers_id, drivers_id, restaurants_id, food_items_id, food_items_quantity, food_items_price, food_name)
+VALUES ('2023-07-03 09:15:00', 1, 2, 1, 1, 1, 12.33, 'miso soup');
+
+INSERT INTO orders (date, customers_id, drivers_id, restaurants_id, food_items_id, food_items_quantity, food_items_price, food_name)
+VALUES ('2023-07-03 10:30:00', 1, 2, 1, 1, 1, 12.33, 'mcchicken');
+
+INSERT INTO orders (date, customers_id, drivers_id, restaurants_id, food_items_id, food_items_quantity, food_items_price, food_name)
+VALUES ('2023-07-04 11:45:00', 1, 2, 2, 1, 3, 12.33, 'sushi');
+
+INSERT INTO orders (date, customers_id, drivers_id, restaurants_id, food_items_id, food_items_quantity, food_items_price, food_name)
+VALUES ('2023-07-04 12:30:00', 1, 2, 2, 1, 3, 12.33, 'karage');
+
+INSERT INTO orders (date, customers_id, drivers_id, restaurants_id, food_items_id, food_items_quantity, food_items_price, food_name)
+VALUES ('2023-07-05 09:00:00', 1, 2, 2, 1, 3, 12.33, 'seaweed salad');
+
+INSERT INTO orders (date, customers_id, drivers_id, restaurants_id, food_items_id, food_items_quantity, food_items_price, food_name)
+VALUES ('2023-07-05 10:15:00', 1, 2, 2, 1, 3, 12.33, 'beef don');
