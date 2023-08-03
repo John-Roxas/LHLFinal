f842067a471e267c33da8baf13ae4afd7ce262b1
Tony Chow <tchow9@gmail.com>
2023-08-03 16:22:28 -0700

    Add proper schema

--- server/db/seeds/test_order_seed.sql
+++ server/db/seeds/test_order_seed.sql
@@ -1,35 +1,14 @@
-INSERT INTO orders (date, customers_id, drivers_id, restaurants_id, food_items_id, food_items_quantity, food_items_price, food_name)
-VALUES ('2023-07-01 10:15:00', 1, 2, 3, 1, 2, 12.33, 'Burger');
 
-INSERT INTO orders (date, customers_id, drivers_id, restaurants_id, food_items_id, food_items_quantity, food_items_price, food_name)
-VALUES ('2023-07-01 11:30:00', 1, 2, 3, 1, 2, 12.33, 'Fries');
+INSERT INTO carts (id, customers_id, closed) VALUES
+(1, 1, FALSE),
+(2, 2, TRUE);
 
-INSERT INTO orders (date, customers_id, drivers_id, restaurants_id, food_items_id, food_items_quantity, food_items_price, food_name)
-VALUES ('2023-07-01 12:45:00', 1, 2, 3, 1, 2, 12.33, 'Nuggets');
+INSERT INTO cart_items (id, cart_id, food_item_id, quantity, food_item_price, food_name) VALUES
+(1, 1, 1, 2, 10.99, 'Burger'),
+(2, 1, 3, 1, 6.49, 'Fries'),
+(3, 2, 2, 3, 8.99, 'Pizza'),
+(4, 2, 4, 2, 3.99, 'Soda');
 
-INSERT INTO orders (date, customers_id, drivers_id, restaurants_id, food_items_id, food_items_quantity, food_items_price, food_name)
-VALUES ('2023-07-02 09:30:00', 1, 2, 3, 1, 2, 12.33, 'Pop');
-
-INSERT INTO orders (date, customers_id, drivers_id, restaurants_id, food_items_id, food_items_quantity, food_items_price, food_name)
-VALUES ('2023-07-02 10:45:00', 1, 2, 1, 1, 1, 12.33, 'pizza');
-
-INSERT INTO orders (date, customers_id, drivers_id, restaurants_id, food_items_id, food_items_quantity, food_items_price, food_name)
-VALUES ('2023-07-02 12:00:00', 1, 2, 1, 1, 1, 12.33, 'garlic bread');
-
-INSERT INTO orders (date, customers_id, drivers_id, restaurants_id, food_items_id, food_items_quantity, food_items_price, food_name)
-VALUES ('2023-07-03 09:15:00', 1, 2, 1, 1, 1, 12.33, 'miso soup');
-
-INSERT INTO orders (date, customers_id, drivers_id, restaurants_id, food_items_id, food_items_quantity, food_items_price, food_name)
-VALUES ('2023-07-03 10:30:00', 1, 2, 1, 1, 1, 12.33, 'mcchicken');
-
-INSERT INTO orders (date, customers_id, drivers_id, restaurants_id, food_items_id, food_items_quantity, food_items_price, food_name)
-VALUES ('2023-07-04 11:45:00', 1, 2, 2, 1, 3, 12.33, 'sushi');
-
-INSERT INTO orders (date, customers_id, drivers_id, restaurants_id, food_items_id, food_items_quantity, food_items_price, food_name)
-VALUES ('2023-07-04 12:30:00', 1, 2, 2, 1, 3, 12.33, 'karage');
-
-INSERT INTO orders (date, customers_id, drivers_id, restaurants_id, food_items_id, food_items_quantity, food_items_price, food_name)
-VALUES ('2023-07-05 09:00:00', 1, 2, 2, 1, 3, 12.33, 'seaweed salad');
-
-INSERT INTO orders (date, customers_id, drivers_id, restaurants_id, food_items_id, food_items_quantity, food_items_price, food_name)
-VALUES ('2023-07-05 10:15:00', 1, 2, 2, 1, 3, 12.33, 'beef don');
+INSERT INTO orders (id, cart_id, date, customers_id, drivers_id, restaurants_id, status, total_amount) VALUES
+(1, 1, '2023-07-03 12:30:00', 1, 1, 1, 'Delivered', 28.46),
+(2, 2, '2023-07-02 18:45:00', 2, NULL, 2, 'Completed', 36.45);
