e02d80de355ce937b04d448351229cdadf35091b
Tony Chow <tchow9@gmail.com>
2023-08-02 17:56:02 -0700

    Fixed code error

--- server/db/seeds/test_order_seed.sql
+++ server/db/seeds/test_order_seed.sql
@@ -1,12 +1,12 @@
 INSERT INTO orders (customers_id, drivers_id, restaurants_id, food_items_id, food_items_quantity, food_items_price, food_name)
-VALUES (1, 2, 3, 1, 2, 3, 12.33, 'Burger')
+VALUES (1, 2, 3, 1, 2, 12.33, 'Burger');
 
 INSERT INTO orders (customers_id, drivers_id, restaurants_id, food_items_id, food_items_quantity, food_items_price, food_name)
-VALUES (1, 2, 3, 1, 2, 3, 12.33, 'Fries')
+VALUES (1, 2, 3, 1, 2, 12.33, 'Fries');
 
 INSERT INTO orders (customers_id, drivers_id, restaurants_id, food_items_id, food_items_quantity, food_items_price, food_name)
-VALUES (1, 2, 3, 1, 2, 3, 12.33, 'Nuggets')
+VALUES (1, 2, 3, 1, 2,12.33, 'Nuggets');
 
 INSERT INTO orders (customers_id, drivers_id, restaurants_id, food_items_id, food_items_quantity, food_items_price, food_name)
-VALUES (1, 2, 3, 1, 2, 3, 12.33, 'Pop')
+VALUES (1, 2, 3, 1, 2, 12.33, 'Pop');
 
