e2e94c5f45de6e201233c339447548ce3a11e82d
Tony Chow <tchow9@gmail.com>
2023-08-03 18:44:48 -0700

    Made customer ID 2 for test data

--- server/db/seeds/test_order_seed.sql
+++ server/db/seeds/test_order_seed.sql
@@ -1,6 +1,6 @@
 
 INSERT INTO carts (id, customers_id, closed) VALUES
-(1, 1, FALSE),
+(1, 2, FALSE),
 (2, 2, TRUE);
 
 INSERT INTO cart_items (id, cart_id, food_item_id, quantity, food_item_price, food_name) VALUES
@@ -10,5 +10,5 @@ INSERT INTO cart_items (id, cart_id, food_item_id, quantity, food_item_price, fo
 (4, 1, 4, 2, 3.99, 'Soda');
 
 INSERT INTO orders (id, cart_id, date, customers_id, drivers_id, restaurants_id, status, total_amount) VALUES
-(1, 1, '2023-07-03 12:30:00', 1, 1, 1, 'Delivered', 28.46),
+(1, 1, '2023-07-03 12:30:00', 2, 1, 1, 'Delivered', 28.46),
 (2, 2, '2023-07-02 18:45:00', 2, NULL, 2, 'Completed', 36.45);
