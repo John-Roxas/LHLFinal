5ef9f568810b8a75526dab0a465536367e5c60d7
Tony Chow <tchow9@gmail.com>
2023-08-03 18:04:12 -0700

    Revised schema and seeds

--- server/db/seeds/test_order_seed.sql
+++ server/db/seeds/test_order_seed.sql
@@ -6,8 +6,8 @@ INSERT INTO carts (id, customers_id, closed) VALUES
 INSERT INTO cart_items (id, cart_id, food_item_id, quantity, food_item_price, food_name) VALUES
 (1, 1, 1, 2, 10.99, 'Burger'),
 (2, 1, 3, 1, 6.49, 'Fries'),
-(3, 2, 2, 3, 8.99, 'Pizza'),
-(4, 2, 4, 2, 3.99, 'Soda');
+(3, 1, 2, 3, 8.99, 'Pizza'),
+(4, 1, 4, 2, 3.99, 'Soda');
 
 INSERT INTO orders (id, cart_id, date, customers_id, drivers_id, restaurants_id, status, total_amount) VALUES
 (1, 1, '2023-07-03 12:30:00', 1, 1, 1, 'Delivered', 28.46),
