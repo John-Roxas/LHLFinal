e345c7425fea67ad245337ac8c2638fd3b787d9d
Tony Chow <tchow9@gmail.com>
2023-08-06 15:02:48 -0700

    Listing cart items in email

--- server/routes/email.js
+++ server/routes/email.js
@@ -5,12 +5,19 @@ const db = require("../db/connection");
 
 router.post("/", (req, res) => {
   console.log(req.body);
-  const email = req.body;
+  const cartItems = req.body;
+  let emailMessage = `Here is your order from ${req.body[0].restaurant_name}:\n\n`;
+  cartItems.forEach((item) => {
+    emailMessage += `Food Name: ${item.food_name}\n`;
+    emailMessage += `Food Items Quantity: ${item.food_items_quantity}\n`;
+    emailMessage += `Price: $${item.food_items_price}\n`;
+    emailMessage += "---\n"; // Separator between items
+  });
   const message = {
     to: process.env.recipientEmail,
     from: process.env.senderEmail,
-    subject: "From Food App",
-    text: JSON.stringify(email),
+    subject: "Your Order Details",
+    text: emailMessage,
   };
   console.log(message);
   sgMail
