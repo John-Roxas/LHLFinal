fad5d82d58217ec4620f8bad2340b7c8102c88fe
Tony Chow <tchow9@gmail.com>
2023-08-06 15:33:05 -0700

    Refined email message to include order total

--- server/routes/email.js
+++ server/routes/email.js
@@ -4,15 +4,17 @@ const sgMail = require("@sendgrid/mail");
 const db = require("../db/connection");
 
 router.post("/", (req, res) => {
-  console.log(req.body);
-  const cartItems = req.body;
-  let emailMessage = `Here is your order from ${req.body[0].restaurant_name}:\n\n`;
+  console.log("This is the req.body format", req.body);
+  const cartItems = req.body.orderData;
+  let orderTotal = parseFloat(req.body.orderTotal).toFixed(2);
+  let emailMessage = `Here is your order from ${req.body.orderData[0].restaurant_name}:\n\n`;
   cartItems.forEach((item) => {
     emailMessage += `Food Name: ${item.food_name}\n`;
     emailMessage += `Food Items Quantity: ${item.food_items_quantity}\n`;
     emailMessage += `Price: $${item.food_items_price}\n`;
     emailMessage += "---\n"; // Separator between items
   });
+  emailMessage += `\nOrder Total: $${orderTotal}`;
   const message = {
     to: process.env.recipientEmail,
     from: process.env.senderEmail,
