3c098379c9753068a4850d1c3c8a75c52dff8dce
TheLoneliestTime <john.a.roxas@gmail.com>
2023-08-07 12:09:17 -0700

    stripe api stuff

--- my-app/src/components/StripePaymentForm.jsx
+++ my-app/src/components/StripePaymentForm.jsx
@@ -8,25 +8,22 @@ const StripePaymentForm = ({ totalAmount, onPaymentSuccess }) => {
   const [isLoading, setLoading] = useState(false);
 
   const handleToken = async (token) => {
-    // You can handle the token here, but since we're not making an actual API call
-    // we can simply proceed with the payment success simulation.
     setLoading(true);
 
-    // Simulate a payment intent (for test purposes)
     const mockPaymentIntent = {
       id: "mock_payment_intent_id",
-      amount: totalAmount * 100, // Amount in cents
+      amount: totalAmount * 100,
       currency: "CAD",
       status: "succeeded",
-      // ... other payment intent details
     };
 
-    // Simulate a successful payment after a short delay (for test purposes)
     setTimeout(() => {
       setLoading(false);
       onPaymentSuccess(mockPaymentIntent);
-    }, 2000); // Simulate a 2-second processing time
+    }, 2000);
   };
+
+  const stripeKey = process.env.REACT_APP_STRIPE_API_KEY; // Access the environment variable
 
   return (
     <>
@@ -34,9 +31,9 @@ const StripePaymentForm = ({ totalAmount, onPaymentSuccess }) => {
 
       <StripeCheckout
         token={handleToken}
-        stripeKey="pk_test_51NOYLPKNHM092Bt6x5egM24zoVt8DopST0EvM6ogZGUXoFqkWVeaT7NUyZpEbekNx7r3BDOyGo5b2Y0h0S9rR1oO00zYlQSqnc" 
+        stripeKey={stripeKey} // Use the environment variable here
         name="DashDine"
-        amount={totalAmount * 100} // Amount in cents (e.g., $10 => 1000 cents)
+        amount={totalAmount * 100}
         currency="CAD"
         image="https://your-company-logo-url.png"
         billingAddress={true}
