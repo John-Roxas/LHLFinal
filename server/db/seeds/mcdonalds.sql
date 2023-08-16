3def16fddae6c9fdb6776935e0561708d9d4fdab
Tony Chow <tchow9@gmail.com>
2023-08-15 19:07:15 -0700

    Add new restaurant seed data

---
+++ server/db/seeds/mcdonalds.sql
@@ -0,0 +1,36 @@
+-- -- A&W Restaurant Info
+
+INSERT INTO restaurants (restaurant_name, email, restaurant_street_address, city, postal_code, phone, category, restaurant_picture)
+VALUES ('McDonalds', 'contact@mcdonalds.com', '7229 Kingsway', 'Burnaby, BC', 'V5E 1G5', '6047181035', 'Fast food', 'https://blog.logomyway.com/wp-content/uploads/2017/01/mcdonalds-logo-1.jpg');
+
+-- -- Mcdonalds food items
+
+INSERT INTO food_items (picture, food_name, description, price, restaurants_id)
+VALUES ('https://d1ralsognjng37.cloudfront.net/d8d60149-176f-4715-b70e-1d4b15ce32c5.jpeg', 'Medium Fries', 'McDonalds World Famous Fries® are made with premium potatoes such as the Russet Burbank and the Shepody.', 4.69, 1);
+
+INSERT INTO food_items (picture, food_name, description, price, restaurants_id)
+VALUES ('https://tb-static.uber.com/prod/image-proc/processed_images/c0931923fe5977940e26997d743f7645/5954bcb006b10dbfd0bc160f6370faf3.jpeg', 'Oreo McFlurry', 'The McFlurry consists of whipped, soft serve McDonalds vanilla-flavored ice cream in a cup.', 4.79, 1);
+
+INSERT INTO food_items (picture, food_name, description, price, restaurants_id)
+VALUES ('https://d1ralsognjng37.cloudfront.net/19daaf93-14df-4e00-b486-73d637ef8423.jpeg', 'Chicken McNuggets', '6 Piece Chicken McNuggets. 100% chicken meat in a deliciously crispy coating, just waiting to be dipped.', 7.09, 1);
+
+INSERT INTO food_items (picture, food_name, description, price, restaurants_id)
+VALUES ('https://d1ralsognjng37.cloudfront.net/af3b45f8-b943-4778-b903-30e98920ee4b.jpeg', 'Big Mac', 'The McDonalds Big Mac® is a 100% beef burger with a taste like no other.', 7.09, 1);
+
+INSERT INTO food_items (picture, food_name, description, price, restaurants_id)
+VALUES ('https://d1ralsognjng37.cloudfront.net/4c511292-4752-4234-9b4c-b3b768f72b89.jpeg', 'Double Cheeseburger', 'The Double Cheeseburger features two 100% pure beef patties seasoned with just a pinch of salt and pepper.', 4.99, 1);
+
+INSERT INTO food_items (picture, food_name, description, price, restaurants_id)
+VALUES ('https://tb-static.uber.com/prod/image-proc/processed_images/4ab8af1118d79cef604e18ac12a7e7cf/5954bcb006b10dbfd0bc160f6370faf3.jpeg', 'Junior Chicken', 'Made with 100% grilled chicken, the Junior Chicken is a burger you can always feel good about.', 3.79, 1);
+
+INSERT INTO food_items (picture, food_name, description, price, restaurants_id)
+VALUES ('https://tb-static.uber.com/prod/image-proc/processed_images/06ce4d87df067ab71d2030d79c2dc394/5954bcb006b10dbfd0bc160f6370faf3.jpeg', 'Happy Meal', 'Happy Meal Crispy Chicken Snack Wrap with Small Fries', 5.67, 1);
+
+INSERT INTO food_items (picture, food_name, description, price, restaurants_id)
+VALUES ('https://tb-static.uber.com/prod/image-proc/processed_images/8c84a4182205a7f5823084e972c89012/5954bcb006b10dbfd0bc160f6370faf3.jpeg', 'Chicken & Bacon McWrap', 'Made with 100% Canadian raised seasoned chicken lettuce, tomato, cucumbers, hickory-smoked bacon and mayo-style sauce.', 8.99, 1);
+
+INSERT INTO food_items (picture, food_name, description, price, restaurants_id)
+VALUES ('https://tb-static.uber.com/prod/image-proc/processed_images/add474e9711370531c03155535ed15d8/5954bcb006b10dbfd0bc160f6370faf3.jpeg', 'Premium Roast Coffee', 'Brewed from 100% Arabica beans, flame-roasted for a rich, delicious full-bodied flavour in your choice of regular or decaf.', 2.29, 1);
+
+INSERT INTO food_items (picture, food_name, description, price, restaurants_id)
+VALUES ('https://tb-static.uber.com/prod/image-proc/processed_images/5806a9bc31e69af258822ba8f8c8716b/5954bcb006b10dbfd0bc160f6370faf3.jpeg', 'McCrispy', 'The McCrispy is a crispy chicken sandwich made with tender and juicy 100% Canadian-raised seasoned chicken.', 7.69, 1);
---
+++ server/db/seeds/sushi_garden.sql
@@ -0,0 +1,36 @@
+-- -- A&W Restaurant Info
+
+INSERT INTO restaurants (restaurant_name, email, restaurant_street_address, city, postal_code, phone, category, restaurant_picture)
+VALUES ('Sushi Garden', 'contact@sushiG.com', '6611 Kingsway ', 'Burnaby, BC', 'V5E 1E2', '6047181035', 'Japanese', 'https://d1ralsognjng37.cloudfront.net/5d6d9e02-ef50-4274-803c-19186ec180f0.jpeg');
+
+-- -- Sushi Garden food items
+
+INSERT INTO food_items (picture, food_name, description, price, restaurants_id)
+VALUES ('https://d1ralsognjng37.cloudfront.net/c70ef553-1a9e-41dc-8bb2-f3a9be7a48cd.jpeg', 'Seafood Salad', 'Served with noodle, Ebi, smoked salmon, masago, crab meat, tako, and wasabi mayo sauce on top.', 13.05, 1);
+
+INSERT INTO food_items (picture, food_name, description, price, restaurants_id)
+VALUES ('https://d1ralsognjng37.cloudfront.net/8eec7859-31bc-4416-a999-9e6081ff535a.jpeg', 'Gyoza', 'Pork and veggies.', 8.75, 1);
+
+INSERT INTO food_items (picture, food_name, description, price, restaurants_id)
+VALUES ('https://d1ralsognjng37.cloudfront.net/736fe567-877f-4756-9e0f-ac5d62487d29.jpeg', 'Spider Roll', 'Softshell crab, cucumber, avocado, mayo, and fish roe with tonkatsu sauce.', 12.95, 1);
+
+INSERT INTO food_items (picture, food_name, description, price, restaurants_id)
+VALUES ('https://d1ralsognjng37.cloudfront.net/22ddc07f-a39f-40de-80ad-1fe67622dbea.jpeg', 'Rainbow Roll', 'Tamago, cucumber, mayo, different raw fish-tuna, salmon, Ebi, hooligan, taco, and Tai.', 12.05, 1);
+
+INSERT INTO food_items (picture, food_name, description, price, restaurants_id)
+VALUES ('https://d1ralsognjng37.cloudfront.net/c52a8879-2067-4b93-9bbb-424fcfe390c7.jpeg', 'Wild Salmon Sushi', 'Japanese dish that showcases the natural flavors of fresh, wild-caught salmon in a simple yet sophisticated presentation. ', 3.85, 1);
+
+INSERT INTO food_items (picture, food_name, description, price, restaurants_id)
+VALUES ('https://d1ralsognjng37.cloudfront.net/b322551f-6ccb-493f-aba9-cbe1d8da5181.jpeg', 'Spicy Salmon Sashimi', 'Vibrant Japanese dish that tantalizes the taste buds with its fresh flavors and fiery kick.', 13.79, 1);
+
+INSERT INTO food_items (picture, food_name, description, price, restaurants_id)
+VALUES ('https://d1ralsognjng37.cloudfront.net/aaf63293-ca3e-4831-8e37-2353ca5c4e19.jpeg', 'DX Assorted Sashimi', 'Japanese culinary offering that showcases an exquisite array of raw seafood.', 53.47, 1);
+
+INSERT INTO food_items (picture, food_name, description, price, restaurants_id)
+VALUES ('https://d1ralsognjng37.cloudfront.net/e41c2b73-8522-472a-9ba5-e6d9441b58cf.jpeg', 'Assorted Tempura', 'One-piece carrot, two pieces zucchini, yam, and three pieces prawn.', 15.89, 1);
+
+INSERT INTO food_items (picture, food_name, description, price, restaurants_id)
+VALUES ('https://d1ralsognjng37.cloudfront.net/05480e4b-dcf6-4383-977e-2aa038018203.jpeg', 'Spicy Chopped Scallop Cone', 'Served with cucumber and spicy sauce. Cone sushi.', 7.55, 1);
+
+INSERT INTO food_items (picture, food_name, description, price, restaurants_id)
+VALUES ('https://d1ralsognjng37.cloudfront.net/d75a090c-b4e6-4718-a6b6-d98b078e1c50.jpeg', 'Unagi Don Rice Bowl', 'Delicious combination of grilled freshwater eel (unagi) served over a bed of steamed white rice.', 23.50, 1);
