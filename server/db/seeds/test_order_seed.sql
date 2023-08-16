b251eb050ac428bb71daac4974a35377ab4e3ca1
Tony Chow <tchow9@gmail.com>
2023-08-16 16:52:32 -0700

    reinstated test order seed

--- server/db/seeds/test_order_seed.sql
+++ server/db/seeds/test_order_seed.sql
@@ -1,19 +1,19 @@
 
--- INSERT INTO carts (customers_id, closed) VALUES
--- (2, TRUE),
--- (2, TRUE);
+INSERT INTO carts (customers_id, closed) VALUES
+(2, TRUE),
+(2, TRUE);
 
--- INSERT INTO cart_items (cart_id, food_item_id, quantity, food_item_price, food_name) VALUES
--- (1, 1, 2, 10.99, 'Burger'),
--- (1, 3, 1, 6.49, 'Fries'),
--- (1, 2, 3, 8.99, 'Pizza'),
--- (1, 4, 2, 3.99, 'Soda');
+INSERT INTO cart_items (cart_id, food_item_id, quantity, food_item_price, food_name) VALUES
+(1, 1, 2, 10.99, 'Burger'),
+(1, 3, 1, 6.49, 'Fries'),
+(1, 2, 3, 8.99, 'Pizza'),
+(1, 4, 2, 3.99, 'Soda');
 
--- INSERT INTO cart_items (cart_id, food_item_id, quantity, food_item_price, food_name) VALUES
--- (2, 1, 2, 10.99, 'Burger'),
--- (2, 3, 1, 6.49, 'Fries'),
--- (2, 2, 3, 8.99, 'Pizza'),
--- (2, 4, 2, 3.99, 'Soda');
+INSERT INTO cart_items (cart_id, food_item_id, quantity, food_item_price, food_name) VALUES
+(2, 1, 2, 10.99, 'Burger'),
+(2, 3, 1, 6.49, 'Fries'),
+(2, 2, 3, 8.99, 'Pizza'),
+(2, 4, 2, 3.99, 'Soda');
 
 INSERT INTO orders (cart_id, date, customers_id, drivers_id, restaurants_id, status, total_amount) VALUES
 (1, '2023-07-03 12:30:00', 2, 1, 1, 'Delivered', 28.46),
