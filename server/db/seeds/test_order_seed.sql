ad5e463d689117904ffce01db9c6cc25416e1023
Tony Chow <tchow9@gmail.com>
2023-08-16 16:51:48 -0700

    Changes test order seed

--- server/db/seeds/test_order_seed.sql
+++ server/db/seeds/test_order_seed.sql
@@ -15,10 +15,10 @@
 -- (2, 2, 3, 8.99, 'Pizza'),
 -- (2, 4, 2, 3.99, 'Soda');
 
--- INSERT INTO orders (cart_id, date, customers_id, drivers_id, restaurants_id, status, total_amount) VALUES
--- (1, '2023-07-03 12:30:00', 2, 1, 1, 'Delivered', 28.46),
--- (1, '2023-07-02 18:45:00', 2, NULL, 2, 'Completed', 36.45),
--- (1, '2023-07-02 18:45:00', 2, NULL, 3, 'Completed', 36.45),
--- (1, '2023-07-02 18:45:00', 2, NULL, 4, 'Completed', 36.45);
+INSERT INTO orders (cart_id, date, customers_id, drivers_id, restaurants_id, status, total_amount) VALUES
+(1, '2023-07-03 12:30:00', 2, 1, 1, 'Delivered', 28.46),
+(1, '2023-07-02 18:45:00', 2, NULL, 2, 'Completed', 27.06),
+(1, '2023-07-02 18:45:00', 2, NULL, 3, 'Completed', 14.31),
+(1, '2023-07-02 18:45:00', 2, NULL, 4, 'Completed', 30.91);
 
 
